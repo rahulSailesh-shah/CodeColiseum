6352becf6d628797ca149d2eb8d9781e7dae09d0
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2024-05-30 22:37:27 -0700

    Monorepo migration

--- frontend/src/store/index.ts
+++
@@ -1,7 +0,0 @@
-import { socketStore } from "./socket";
-import { codeStore } from "./code";
-import { userStore } from "./user";
-
-export const useSocketStore = socketStore;
-export const useCodeStore = codeStore;
-export const useUserStore = userStore;
--- worker/src/messages.ts
+++
@@ -1,3 +0,0 @@
-export const CODE_QUEUE = "code_queue";
-export const SUBMISSION_RESULT = "submission_result";
-export const SUBMISSION_TOKEN = "submission_token";
